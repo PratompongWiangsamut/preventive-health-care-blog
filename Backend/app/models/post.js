6ee9b8bee1c585f1f256caffb7dab1bc0496fefa
pratompongwiangsamut <pratompong_wiangsamu@cmu.ac.th>
2020-09-03 13:35:59 +0700

    test

--- Backend/app/models/post.js
+++ Backend/app/models/post.js
@@ -9,10 +9,7 @@ module.exports = (sequelize, Sequelize)=>{
             type: Sequelize.STRING(40),
             allowNull: false
         },
-        tag: {
-            type: Sequelize.STRING(40),
-            allowNull: false
-        },
+        
         tex: {
             type: Sequelize.STRING(40),
             allowNull: false
@@ -24,8 +21,3 @@ module.exports = (sequelize, Sequelize)=>{
     })
     return Post
 }
-/*this.title = post.title;
-this.tag = post.tag;
-this.tex = post.tex;
-this.belong = post.belong;
-this.rank = post.rank;*/
