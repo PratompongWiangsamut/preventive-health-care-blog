94fdea2ac95dbc847b1523f66ad68f3e43c3b9e5
pratompongwiangsamut <pratompong_wiangsamu@cmu.ac.th>
2020-09-25 20:08:35 +0700

    add get all post by use API

--- Frontend/src/Component/FrontEnd/Home/Post/Posts.js
+++ Frontend/src/Component/FrontEnd/Home/Post/Posts.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 import { Card, Button, } from "react-bootstrap";
+import axios from 'axios'
 
 export default class Tables extends Component {
   state = {
     showmodal: false,
+    post: []
   };
+
+  componentDidMount(){
+    axios.get('http://localhost:3000/api/post/post').then((res)=>{
+      console.log('pre-data: ', res.data)
+      this.setState({post: res.data})
+      console.log('post-data: ', this.state.post)
+    })
+  }
 
   showmodalHandler = () => {
     this.setState({ showmodal: true });
@@ -14,6 +24,20 @@ export default class Tables extends Component {
     this.setState({ showmodal: false });
   };
   render() {
+    var posts = this.state.post.map((item)=>
+    <Card className="text-center" key={item.pid}>
+        <Card.Header>Featured</Card.Header>
+        <Card.Body>
+          <Card.Title>{item.title}</Card.Title>
+          <Card.Text>
+            {item.tex}
+          </Card.Text>
+          <Button variant="primary">Read more</Button>
+        </Card.Body>
+  <Card.Footer className="text-muted">{item.tag}</Card.Footer>
+      </Card>
+  
+    )
     return (
       <div >
         <div
@@ -23,20 +47,10 @@ export default class Tables extends Component {
             alignItems: "center",
           }}
         >
-        <Card className="text-center">
-          <Card.Header>Featured</Card.Header>
-          <Card.Body>
-            <Card.Title>Special title treatment</Card.Title>
-            <Card.Text>
-              With supporting text below as a natural lead-in to additional content.
-            </Card.Text>
-            <Button variant="primary">Read more</Button>
-          </Card.Body>
-          <Card.Footer className="text-muted">2 days ago</Card.Footer>
-        </Card>
+        
         </div>
 
-        
+        {posts}
       </div>
     );
   }
